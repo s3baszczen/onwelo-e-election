8e96ab2c85c3063c3a98b02185f7994f52cf022f
sebastian <sebastianszczebiot@gmail.com>
2026-01-14 13:12:24 +0100

    initialize springboot-based election service

---
+++ src/main/java/com/onwelo/election/EElectionApplication.java
@@ -0,0 +1,13 @@
+package com.onwelo.election;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EElectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EElectionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/onwelo/election/EElectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onwelo.election;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EElectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
